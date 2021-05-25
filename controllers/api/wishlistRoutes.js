fbcfe83a0d09c2292e48dc6b2edfb87cdecafcc9
Jonathan Rein <jrein1296@gmail.com>
2021-05-25 17:31:38 -0400

    Updates to wishlistRoutes.js.

--- controllers/api/wishlistRoutes.js
+++ controllers/api/wishlistRoutes.js
@@ -64,7 +64,6 @@ router.post("/", async (req, res) => {
   try {
     const wishlistEntry = await Wishlist.create({
       user_name: req.session.user_name,
-=======
       wishlist_text: req.body.wishlistEntry
     });
     res.status(200).json(wishlistEntry);
