e26d984d4c804be7bde1939ac2bae6874716519a
Jim Harris <arcangyl@gmail.com>
2021-05-30 20:18:39 -0400

    Added the update and delete wishlist items scripts, routes and handlebars

--- controllers/api/wishlistRoutes.js
+++ controllers/api/wishlistRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const e = require("express");
 const { Wishlist } = require("../../models");
 const { User } = require("../../models");
 
@@ -14,20 +15,20 @@ router.get("/", async (req, res) => {
 
 
 // get single wishlist by id and return selected wishlist as JSON payload
-router.get("/:id", async (req, res) => {
-  try {
-    const selectedWishlist = await Wishlist.findOne({
-      where: { id: req.params.id },
-    });
-    if (!selectedWishlist) {
-      res.status(404).json({ message: "Wishlist does not exist" });
-      return;
-    }
-    res.status(200).json(selectedWishlist);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const selectedWishlist = await Wishlist.findOne({
+//       where: { id: req.params.id },
+//     });
+//     if (!selectedWishlist) {
+//       res.status(404).json({ message: "Wishlist does not exist" });
+//       return;
+//     }
+//     res.status(200).json(selectedWishlist);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // post a new wishlist item to the database
 // this is the api/wishlist/ endpoint
@@ -51,6 +52,28 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const editWishItem = await Wishlist.findByPk(req.params.id);
+    if (editWishItem) {
+      const item = editWishItem.wishlist_text;
+      const wishId = editWishItem.id;
+      const usrID = editWishItem.user_id;
+      res.render('wishlistUpdate', {
+        layout: 'main',
+        item,
+        wishId,
+        usrID,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err)  {
+    console.log(err);
+      res.redirect('/wishlist');
+  }
+});
+
 
 // update a wishlist by id and return updated wishlist as JSON payload.  this is the /api/wishlist/:id endpoint
 /* put should look like this...
@@ -61,16 +84,16 @@ router.post("/", async (req, res) => {
     */
 router.put("/:id", async (req, res) => {
   try {
-    const updateWishlist = await Wishlist.update(req.body, {
+    const [updatedRows] = await Wishlist.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    if (!updateWishlist[0]) {
-      res.status(404).json({ message: "Wishlist not found." });
-      return;
+    if (updatedRows > 0) {
+       res.status(200).end();
+    } else {
+      res.status(404).end();
     }
-    res.status(200).json(updateWishlist);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -79,16 +102,16 @@ router.put("/:id", async (req, res) => {
 // delete a wishlist by its `id` value.  this is the /api/wishlist/:id endpoint
 router.delete("/:id", async (req, res) => {
   try {
-    const wishlistDelete = await Wishlist.destroy({
+    const [deletedRows] = await Wishlist.destroy({
       where: {
         id: req.params.id,
       },
     });
-    if (!wishlistDelete) {
-      res.status(404).json({ message: "Wishlist not found" });
-      return;
+    if (deletedRows > 0) {
+      res.status(200).end();
+    } else {
+    res.status(404).end();
     }
-    res.status(200).json(wishlistDelete);
   } catch (err) {
     res.status(500).json(err);
   }
